c8276ff761501e10512af5d03b6d9591e0449539
Furkan <furkan.simsek@student.hpi.uni-potsdam.de>
2021-05-14 18:43:01 +0200

    Add implememtation to ReferenceSegment Type and extend TableScan.

--- src/lib/storage/reference_segment.hpp
+++ src/lib/storage/reference_segment.hpp
@@ -22,20 +22,41 @@ class ReferenceSegment : public BaseSegment {
   // creates a reference segment
   // the parameters specify the positions and the referenced segment
   ReferenceSegment(const std::shared_ptr<const Table>& referenced_table, const ColumnID referenced_column_id,
-                   const std::shared_ptr<const PosList>& pos);
+                   const std::shared_ptr<const PosList>& pos) : _referenced_table(referenced_table), _referenced_column_id(referenced_column_id),
+                    _position_list(pos){};
 
-  AllTypeVariant operator[](const ChunkOffset chunk_offset) const override;
+  AllTypeVariant operator[](const ChunkOffset chunk_offset) const override {
+    auto position = _position_list->at(chunk_offset);
+
+    // TODO:Refactoring neccessary
+    return (*(_referenced_table->get_chunk(position.chunk_id)).get_segment(_referenced_column_id))[position.chunk_offset];
+  };
 
   void append(const AllTypeVariant&) override { throw std::logic_error("ReferenceSegment is immutable"); };
 
-  ChunkOffset size() const override;
+  ChunkOffset size() const override {
+      return static_cast<ChunkOffset>(_position_list->size());
+  }
 
-  const std::shared_ptr<const PosList>& pos_list() const;
-  const std::shared_ptr<const Table>& referenced_table() const;
+  const std::shared_ptr<const PosList>& pos_list() const {
+      return _position_list;
+  }
+  const std::shared_ptr<const Table>& referenced_table() const {
+      return _referenced_table;
+  }
 
-  ColumnID referenced_column_id() const;
+  ColumnID referenced_column_id() const {
+      return _referenced_column_id;
+  }
 
-  size_t estimate_memory_usage() const final;
+  size_t estimate_memory_usage() const final {
+    return (sizeof(RowID) * _position_list->size());
+  }
+
+ protected:
+  const std::shared_ptr<const Table> _referenced_table;
+  const ColumnID _referenced_column_id;
+  const std::shared_ptr<const PosList> _position_list;
 };
 
 }  // namespace opossum
