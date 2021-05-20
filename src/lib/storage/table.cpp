fad24eaa7dd0826934a4238926d743a78de1404e
phoeinx <hradilak@uni-potsdam.de>
2021-05-20 18:52:35 +0200

    Refactor: Update emplace_back usage to be able to obtain performance improvements

--- src/lib/storage/table.cpp
+++ src/lib/storage/table.cpp
@@ -119,9 +119,8 @@ void Table::compress_chunk(ChunkID chunk_id) {
   for (auto column = 0; column < chunk_column_count; column++) {
     const auto segment_type = _column_types[column];
     auto segment = chunk.get_segment(static_cast<ColumnID>(column));
-
-    threads.emplace_back(
-        std::thread(&Table::_add_dictionary_segment_to_chunk, this, std::ref(compressed_chunk), segment_type, segment));
+    
+    threads.emplace_back(&Table::_add_dictionary_segment_to_chunk, this, std::ref(compressed_chunk), segment_type, segment);
   }
 
   for (auto& thread : threads) {
