f8579deface4189e4dd3542c587417347f2ec401
phoeinx <hradilak@uni-potsdam.de>
2021-05-21 14:31:50 +0200

    Refactor Fixed Size Attribute Vector, estimate_memory in value segment

--- src/lib/storage/fixed_size_attribute_vector.hpp
+++ src/lib/storage/fixed_size_attribute_vector.hpp
@@ -14,29 +14,22 @@ class FixedSizeAttributeVector : public BaseAttributeVector {
   explicit FixedSizeAttributeVector(size_t attribute_vector_size) {
     _attributes = std::vector<uintX_t>(attribute_vector_size);
   }
-  virtual ~FixedSizeAttributeVector() = default;
-
-  // we need to explicitly set the move constructor to default when
-  // we overwrite the copy constructor
-  FixedSizeAttributeVector(FixedSizeAttributeVector&&) = default;
-  FixedSizeAttributeVector& operator=(FixedSizeAttributeVector&&) = default;
-
   // returns the value id at a given position
-  ValueID get(const size_t position) const { return static_cast<ValueID>(_attributes.at(position)); }
+  ValueID get(const size_t position) const override { return static_cast<ValueID>(_attributes.at(position)); }
 
   // sets the value id at a given position
-  void set(const size_t position, const ValueID value_id) {
+  void set(const size_t position, const ValueID value_id) override {
     DebugAssert(position >= 0 && position < _attributes.size(), "Cannot set value at invalid position");
     _attributes[position] = static_cast<uintX_t>(value_id);
   }
 
   // returns the number of values
-  size_t size() const { return _attributes.size(); }
+  size_t size() const override { return _attributes.size(); }
 
   // returns the width of biggest value id in bytes
-  AttributeVectorWidth width() const { return static_cast<AttributeVectorWidth>(sizeof(uintX_t)); }
+  AttributeVectorWidth width() const override { return static_cast<AttributeVectorWidth>(sizeof(uintX_t)); }
 
  private:
-  std::vector<uintX_t> _attributes = {};
+  std::vector<uintX_t> _attributes;
 };
 }  // namespace opossum
