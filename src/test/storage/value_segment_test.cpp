436178ad3e13fa71a8c139d6acfe812acb81e8fe
marc-rosenau <marc@mrdeliver.de>
2021-05-21 11:40:19 +0200

    Refactor compress chunk

--- src/test/storage/value_segment_test.cpp
+++ src/test/storage/value_segment_test.cpp
@@ -60,11 +60,11 @@ TEST_F(StorageValueSegmentTest, AccessValueAtIndex) {
   EXPECT_THROW(double_value_segment[1], std::exception);
 }
 
-// TEST_F(StorageValueSegmentTest, MemoryUsage) {
-//   int_value_segment.append(1);
-//   EXPECT_EQ(int_value_segment.estimate_memory_usage(), size_t{4});
-//   int_value_segment.append(2);
-//   EXPECT_EQ(int_value_segment.estimate_memory_usage(), size_t{8});
-// }
+TEST_F(StorageValueSegmentTest, MemoryUsage) {
+  int_value_segment.append(1);
+  EXPECT_EQ(int_value_segment.estimate_memory_usage(), size_t{4});
+  int_value_segment.append(2);
+  EXPECT_EQ(int_value_segment.estimate_memory_usage(), size_t{8});
+}
 
 }  // namespace opossum
